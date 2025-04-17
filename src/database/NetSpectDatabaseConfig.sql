c8f8e88c292abfbb37724b510ebcb3dbca566867
Shay Hahiashvili <shayhh@outlook.com>
2025-04-17 18:00:31 +0300

    Updated database config file.

--- src/database/NetSpectDatabaseConfig.sql
+++ src/database/NetSpectDatabaseConfig.sql
@@ -14,14 +14,16 @@ IF OBJECT_ID('Alerts', 'U') IS NOT NULL DROP TABLE Alerts;
 
 -- Create Users table
 CREATE TABLE Users (
-    userId INT IDENTITY(1,1) UNIQUE NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    userName VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
+	userId INT IDENTITY(1,1) UNIQUE NOT NULL,
+	email VARCHAR(255) UNIQUE NOT NULL,
+	userName VARCHAR(255) UNIQUE NOT NULL,
+	password VARCHAR(255) NOT NULL,
 	lightMode INT NOT NULL DEFAULT 0, --represents the color mode of app, 1 means lightmode, else darkmode
 	isDeleted INT NOT NULL DEFAULT 0, --represents state of account, if 1 its deleted, else not
 	CHECK (email LIKE '_%@_%._%'),
 	CHECK (LEN(userName) BETWEEN 4 AND 16),
+	CHECK (lightMode BETWEEN 0 AND 1),
+	CHECK (isDeleted BETWEEN 0 AND 1),
 	PRIMARY KEY(userId)
 );
 
@@ -29,19 +31,19 @@ CREATE TABLE Users (
 -- Create Blacklist table
 CREATE TABLE Blacklist (
 	userid INT NOT NULL,
-    macAddress VARCHAR(255) NOT NULL,
+	macAddress VARCHAR(255) NOT NULL,
 	PRIMARY KEY(userId, macAddress),
-    FOREIGN KEY (userId) REFERENCES Users(userId)
+	FOREIGN KEY (userId) REFERENCES Users(userId)
 );
 
 
 -- Create Alerts table
 CREATE TABLE Alerts (
 	alertId INT IDENTITY(1,1) UNIQUE NOT NULL,
-    userId INT NOT NULL,
+	userId INT NOT NULL,
 	interface VARCHAR(255) NOT NULL,
 	attackType VARCHAR(255) NOT NULL,
-    sourceIp VARCHAR(255) NOT NULL,
+	sourceIp VARCHAR(255) NOT NULL,
 	sourceMac VARCHAR(255) NOT NULL,
 	destinationIp VARCHAR(255) NOT NULL,
 	destinationMac VARCHAR(255) NOT NULL,
@@ -50,8 +52,9 @@ CREATE TABLE Alerts (
 	timestamp VARCHAR(255) NOT NULL, -- timestamp template is hh:mm:ss dd/mm/yy
 	isDeleted INT NOT NULL DEFAULT 0, --represents state of alert, if 1 its deleted, else not
 	CHECK (timestamp LIKE '[0-2][0-9]:[0-5][0-9]:[0-5][0-9] [0-3][0-9]/[0-1][0-9]/[0-9][0-9]'),
-    PRIMARY KEY(alertId),
-    FOREIGN KEY (userId) REFERENCES Users(userId)
+	CHECK (isDeleted BETWEEN 0 AND 1),
+	PRIMARY KEY(alertId),
+	FOREIGN KEY (userId) REFERENCES Users(userId)
 );
 
 

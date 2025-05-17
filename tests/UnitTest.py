b4c84cf2edab1260b89a224dc3f422980f79f59b
Shay Hahiashvili <shayhh@outlook.com>
2025-05-17 23:56:07 +0300

    In UnitTest, added remaining unit tests for the rest of the functions in MainFunctions and added more unit tests for several functions from NetSpect.

--- tests/UnitTest.py
+++ tests/UnitTest.py
@@ -1,7 +1,41 @@
 import sys, os, time, random, pytest
 from PySide6.QtCore import Qt
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))) #ensures that UnitTest.py file will run from main folder in the terminal
-from src.main.MainFunctions import *
+# ensures that UnitTest.py file will run from main folder in the terminal
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.append(os.path.join(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')), 'src', 'main'))
+from src.main.NetSpect import *
+
+
+# test step function for ToSHA256 function
+@pytest.mark.parametrize('message', [('Test message', 'c0719e9a8d5d838d861dc6f675c899d2b309a3a65bb9fe6b11e5afcbf9a2c0b1'), ('Unit test message', 'd7339ac2986b8c29733af74a6ddb9bc7f0b6cebe2c73be4c50c3779b14107bad'), ('Message testing', 'c61a658992579824833b927d78989cfca1a164ea9abed0fc1a8cf493f379352b')])
+def testToSHA256(message):
+    # get the hashed message with function
+    hashedMessage = NetSpect.ToSHA256(message[0])
+
+    # assert function was successful and returned correct hash
+    assert hashedMessage == message[1], f'Expected hash was {message[1]}, got {hashedMessage}'
+
+
+# test step function for GetPassword function
+@pytest.mark.parametrize('length', [8, 16, 32])
+def testGetPassword(length):
+    # get the password with function
+    password = NetSpect.GetPassword(length)
+
+    # assert function was successful and returned correct password with expected length, with at least one uppercase and one digit
+    assert len(password) == length, f'Password length mismatch: expected {length}, got {len(password)}.'
+    assert any(c.isupper() for c in password), f'Password "{password}" must contain at least one uppercase letter.'
+    assert any(c.isdigit() for c in password), f'Password "{password}" must contain at least one digit.'
+
+
+# test step function for GetResetCode function
+@pytest.mark.parametrize('length', [8, 16, 32])
+def testGetResetCode(length):
+    # get reset password code with function
+    resetCode = NetSpect.GetResetCode(length)
+
+    # assert function was successful and returned reset code in desired length
+    assert len(resetCode) == length, f'Reset code length mismatch: expected {length}, got {len(resetCode)}.'
 
 
 # test step function for GetNetworkInterfaces function
@@ -14,26 +48,132 @@ def testGetNetworkInterfaces():
     assert availableInterfaces != {} and firstInterface != None, 'Received an empty interface dictionary, no interfaces were found.'
 
 
+# test step function for GetNetmaskFromIp function positive attempt
+def testGetNetmaskFromIpPositive():
+    # get interfaces with QNetwork
+    interfaces = QNetworkInterface.allInterfaces()
+
+    # get all available network interfaces with our function
+    availableInterfaces = NetworkInformation.GetNetworkInterfaces()
+
+    # get first available network interface
+    firstInterface = next(iter(availableInterfaces.values()), None)
+
+    # get ipv4 address list from first interface
+    ipv4Addresses = firstInterface.get('ipv4Addrs') if firstInterface else []
+
+    # assert that we received an initialized ipv4 addresses list
+    assert ipv4Addresses != [], 'Received an empty ipv4 list, no ipv4 addresses were found.'
+
+    # get netmask from first ip address
+    netmask = NetworkInformation.GetNetmaskFromIp(interfaces, ipv4Addresses[0])
+
+    # assert function was successful and retrived a netmask for the first ip
+    assert netmask != None, 'Failed receiving a netmask for ipv4 address.'
+
+
+# test step function for GetNetmaskFromIp function negative attempt
+def testGetNetmaskFromIpNegative():
+    # get interfaces with QNetwork
+    interfaces = QNetworkInterface.allInterfaces()
+
+    # represents a random ipv4 address
+    ipv4Address = '.'.join(str(random.randint(0, 255)) for _ in range(4))
+
+    # get netmask from random ip address
+    netmask = NetworkInformation.GetNetmaskFromIp(interfaces, ipv4Address)
+
+    # assert function was successful and didnt receive a netmask
+    assert netmask == None, 'Received a netmask for invalid ipv4 address.'
+
+
+# test step function for GetMTUFromInterface function positive attempt
+def testGetMTUFromInterfacePositive():
+    # get interfaces with QNetwork
+    interfaces = QNetworkInterface.allInterfaces()
+
+    # get all available network interfaces with our function
+    availableInterfaces = NetworkInformation.GetNetworkInterfaces()
+
+    # get first available network interface
+    firstInterface = next(iter(availableInterfaces.values()), None)
+
+    # get interface name of first interface
+    interfaceName = firstInterface.get('name') if firstInterface else ''
+
+    # assert that we received a valid interface name
+    assert interfaceName != '', 'Failed receiving interface name.'
+
+    # get mtu from the first interface
+    interfaceMTU = NetworkInformation.GetMTUFromInterface(interfaces, interfaceName)
+
+    # assert function was successful and retrived mtu for the first interface
+    assert interfaceMTU != None, 'Failed receiving mtu for interface.'
+
+
+# test step function for GetMTUFromInterface function negative attempt
+def testGetMTUFromInterfaceNegative():
+    # get interfaces with QNetwork
+    interfaces = QNetworkInterface.allInterfaces()
+
+    # represents a random interface name
+    interfaceName = 'Ethernet172'
+
+    # get mtu from random interface name
+    interfaceMTU = NetworkInformation.GetMTUFromInterface(interfaces, interfaceName)
+
+    # assert function was successful and didnt receive mtu
+    assert interfaceMTU == None, 'Received mtu for invalid interface name.'
+
+
+# test step function for CompareTimepstemps function positive attempts
+@pytest.mark.parametrize('timestamps', [('22:00:10 01/05/24', '22:10:10 01/05/24', 10), ('09:10:00 10/05/24', '09:25:00 10/05/24', 15), ('07:21:45 05/05/24', '08:21:45 05/05/24', 60)])
+def testCompareTimepstempsPositive(timestamps):
+    # assert function was successful and compared the timestamps correctly
+    assert NetworkInformation.CompareTimepstemps(timestamps[0], timestamps[1], timestamps[2])  == True, f'Expected {timestamps[2]} minutes between {timestamps[0]} and {timestamps[1]}, but function returned False.'
+
+
+# test step function for CompareTimepstemps function negative attempts
+@pytest.mark.parametrize('timestamps', [('20:30:55 02/05/24', '20:35:55 02/05/24', 15), ('09:00:00 04/05/24', '09:30:00 04/05/24', 45), ('07:00:00 03/05/24', '07:30:00 03/05/24', 60)])
+def testCompareTimepstempsNegative(timestamps):
+    # assert function was successful and compared the timestamps correctly
+    assert NetworkInformation.CompareTimepstemps(timestamps[0], timestamps[1], timestamps[2])  == False, f'Expected mismatch for {timestamps[2]} minutes between {timestamps[0]} and {timestamps[1]}, but function returned True.'
+
+
 # test step function for GuidToStr function
 def testGuidToStrPositive():
-    # hard code a valid guid string
-    guid = '{07444BF8-F269-473F-B278-891AA8D81C6E}'
+    # test only for Windows machines
+    if sys.platform.startswith('win32'):
+        # get a list of the network interfaces
+        interfaces = get_if_list()
 
-    # call the translate function with given guid
-    output = NetworkInformation.GuidToStr(guid)
+        # assert that we received an initialized ipv4 addresses list
+        assert interfaces != [], 'Received an empty interfaces list, no guid were found.'
 
-    # assert that the given guid is valid by check that the output is not equal to the input
-    assert output != guid, f'Failed to translate the guid. Input: {guid}, Output: {output}.'
+        # get the first guid in interfaces
+        guid = interfaces[0]
+
+        # call the translate function with given guid
+        output = NetworkInformation.GuidToStr(guid)
+
+        # assert that the given guid is valid by check that the output is not equal to the input
+        assert output != guid, f'Failed to translate the guid. Input: {guid}, Output: {output}.'
+    else:
+        assert True
 
 
 # test step function for GuidToStr function
 @pytest.mark.parametrize('guid', ['07444BF8-F269-473F-B278-891AA8D81C6E', '{473F-B278-891AA8D81C6E}', 'not a guid'])
 def testGuidToStrNegative(guid):
-    # call the translate function with given guid
-    output = NetworkInformation.GuidToStr(guid)
+    # test only for Windows machines
+    if sys.platform.startswith('win32'):
+        # call the translate function with given guid
+        output = NetworkInformation.GuidToStr(guid)
 
-    # assert that the given guid is invalid by check that the output is equal to the input
-    assert output == guid, f'Was able to translate an invalid guid. Input: {guid}, Output: {output}.'
+        # assert that the given guid is invalid by check that the output is equal to the input
+        assert output == guid, f'Was able to translate an invalid guid. Input: {guid}, Output: {output}.'
+    else:
+        assert True
 
 
 # test step function for GetSystemInformation function
@@ -60,7 +200,7 @@ def testSaveFlowsInFile():
     assert os.path.isfile(fileName), f'File with name: {fileName} does not exist.'
 
     # assert that the contents of the file matches the input data
-    with open(fileName, "r", encoding="utf-8") as file:
+    with open(fileName, 'r', encoding='utf-8') as file:
         contents = file.read()
     os.remove(fileName) #delete the file before checking if the contents is valid
 
@@ -83,7 +223,7 @@ def testSaveCollectedData():
     assert os.path.isfile(fileName), f'File with name: {fileName} does not exist.'
     
     # assert that the contents of the file matches the input data
-    with open(fileName, "r", encoding="utf-8") as file:
+    with open(fileName, 'r', encoding='utf-8') as file:
         contents = file.read()
     os.remove(fileName) #delete the file before checking if the contents is valid
 
